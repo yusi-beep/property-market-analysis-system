00e6a8019f6ce4f9000d9074c015b4b89cd11462
Yusuf Yusufov <yusitoo1234@gmail.com>
2022-02-04 11:01:30 +0200

    Formtat extractet data

--- src/main/java/com/real/estate/analyzer/app/ExtractDataforOneAdvert.java
+++ src/main/java/com/real/estate/analyzer/app/ExtractDataforOneAdvert.java
@@ -15,17 +15,23 @@ public class ExtractDataforOneAdvert {
 		
 		String urlImotBg =  "https://www.imot.bg/pcgi/imot.cgi?act=5&adv=1b164382325993161";
 		
-		//extractDataFromImotBG(driver, urlImotBg);
+		extractDataFromImotBG(driver, urlImotBg);
 		
 		System.out.println("<---------------------------->");
 		
-		String urlHomesBg = "https://www.homes.bg/offer/apartament-za-prodazhba/tristaen-89m2-sofiya-zhk.-ljulin-6/as1370158";
+		String urlHomesBg = "https://www.homes.bg/offer/apartament-za-prodazhba/"
+				+ "tristaen-89m2-sofiya-zhk.-ljulin-6/as1370158";
 		
 		extractDataFromHomesBg(driver, urlHomesBg);
 	}
 	
-	private static void extractDataFromHomesBg(WebDriver driver, String urlHomesBg) throws InterruptedException {
+	private static void extractDataFromHomesBg(
+			WebDriver driver, String urlHomesBg) throws InterruptedException {
 
+		String[] parts;
+		
+		String splitAt = ",";
+		
 		driver.get(urlHomesBg);
 		Thread.sleep(200);
 		
@@ -33,41 +39,77 @@ public class ExtractDataforOneAdvert {
 				.className("section-title"))
 				.getText();
 		
+		parts = splitStringAt(title, splitAt);
+		
+		title = parts[0];
+		
+		String squareFootage = parts[1].trim();
+		
 		String price = driver.findElement(By
-				.xpath("/html/body/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/table/tbody/tr/td[2]/table/tbody/tr[1]/td"))
+				.xpath("/html/body/div/div/div[2]/div[2]/div/div/div/div/div/"
+						+ "div[2]/div[2]/table/tbody/tr/td[2]/table/tbody/tr[1]/td"))
 				.getText();
+		parts = splitStringAt(price, splitAt);
+		price = parts[0] + " " + parts[1];
 		
 		String floor = driver.findElement(By
-				.xpath("/html/body/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div[1]/div/div[2]/span/h3"))
-				.getText();
+				.xpath("/html/body/div/div/div[2]/div[2]/div/div/div/div/div/"
+						+ "div[2]/div[2]/div[1]/div/div[2]/span/h3"))
+				.getText()
+				.substring(0,1);
 		
 		String address = driver.findElement(By
-				.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/table/tbody/tr/td[1]"))
+				.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/"
+						+ "div[2]/div[2]/table/tbody/tr/td[1]"))
 				.getText();
+		
+		parts = splitStringAt(address, splitAt);
+		
+		address = parts[0];
+		
+		String city = parts[1]
+				.trim();
 		
 		String broker = driver.findElement(By
-				.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div[2]/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td/div[1]"))
+				.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div/div/div/"
+						+ "div[2]/div[2]/div[2]/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td/div[1]"))
 				.getText();
 		
 		
-		System.out.println(title+ "\n" + address + "\n" + price + "\n" + floor + "\n" + broker);
+		System.out.printf("%s%n%s%n %s%n %s%n %s%n %s%n %s%n %s%n",
+				"Homes.bg", title, address, squareFootage, city, price, floor, broker);
 	}
 
-	public static void extractDataFromImotBG(WebDriver driver, String url ) throws InterruptedException {
+	public static void extractDataFromImotBG(
+			WebDriver driver, String url ) throws InterruptedException {
+		
+		String[] parts;
+		
+		String splitAt = ",";
+		
+		int delay= 200;
 		
 		driver.get(url);
-		Thread.sleep(200);
+		Thread.sleep(delay);
 		
 		driver.findElement(By.className("fc-button-label")).click();
-		Thread.sleep(200);
+		Thread.sleep(delay);
 		
-		String offer = driver.findElement(By
+		String title = driver.findElement(By
 				.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]/form[2]/div[2]/strong"))
 				.getText();
 		
-		String place = driver.findElement(By
+		String address = driver.findElement(By
 				.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]/form[2]/div[2]/span[1]"))
 				.getText();
+		
+		parts = splitStringAt(address, splitAt);
+		
+		address = parts[1]
+				.trim();
+		
+		String city = parts[0]
+				.substring(5);
 		
 		String price = driver.findElement(By
 				.id("cena"))
@@ -76,14 +118,24 @@ public class ExtractDataforOneAdvert {
 		String squareFootage = driver.findElement(By
 				.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]/form[2]/div[2]/ul/li[2]"))
 				.getText();
+		
 		String floor = driver.findElement(By
 				.xpath("/html/body/div[2]/table/tbody/tr[1]/td[1]/form[2]/div[2]/ul/li[4]"))
-				.getText();
+				.getText()
+				.substring(0,1);
 		
 		String broker = driver.findElement(By
 				.xpath("/html/body/div[2]/table/tbody/tr[1]/td[2]/div[1]/div/div/a[2]"))
 				.getText();
 		
-			System.out.printf("%s%n %s%n %s%n %s%n %s%n %s%n", offer, place, price, squareFootage, floor, broker);
+			System.out.printf("%s%n%s%n %s%n %s%n %s%n %s%n %s%n %s%n",
+					"Imot.bg", title, address, city, price, squareFootage, floor, broker);
+	}
+	
+	public static String[] splitStringAt(String str, String splitAt) {
+		
+		String[] parts = str.split(splitAt);
+		
+		return parts;
 	}
 }
