f74df9e47de17faa8b4f1fcb7473570c3c990c94
Yusuf Yusufov <yusitoo1234@gmail.com>
2022-05-15 12:48:02 +0300

    Add method for agency

--- src/main/java/com/real/estate/analyzer/controllers/HomeController.java
+++ src/main/java/com/real/estate/analyzer/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.real.estate.analyzer.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,28 +10,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.real.estate.analyzer.repository.CityRepository;
 import com.real.estate.analyzer.service.AdvertServiceImpl;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
     private AdvertServiceImpl advertServiceImpl;
 	
-	@RequestMapping(value="/1", method=RequestMethod.GET)
-	public String neighbourhoodAnalysis(Model theModel) {
-		theModel.addAttribute("neighbourhood", advertServiceImpl.minPriceForNeighbourhood("Добруджански"));
+//	@Autowired
+//	private CityRepository cityRepository;
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String cityList(Model theModel) {
+		List<String> cityList = new ArrayList<String>();
+		cityList.add("Шумен");
+		cityList.add("Варна");
+		cityList.add("Бургас");
+		cityList.add("София");
+		
+//		cityList = cityRepository.cityList();
+		
+		theModel.addAttribute("cityOptions", cityList);
+		return "homepage";
+	}
+	
+	@RequestMapping(value = "/1", method = RequestMethod.GET)
+	public String neighbourhoodAnalysis(@RequestParam(value="cityIn", required = true) String cityIn,
+			@RequestParam(value="neighbourhoodIn", required = false) String neighbourhoodIn, Model theModel) {
+		
+		theModel.addAttribute("city", advertServiceImpl.minPriceForNeighbourhood(cityIn));
+		theModel.addAttribute("neighbourhood", advertServiceImpl.minPriceForNeighbourhood(neighbourhoodIn));
 		return "homepage";
 	}
 	
 	@RequestMapping(value="/2", method=RequestMethod.GET)
-	public String cityAnalysis(Model theModel) {
-		theModel.addAttribute("city", advertServiceImpl.minPriceForNeighbourhood("Шумен"));
+	public String agencyAnalysis(@RequestParam(value="agencyIn", required = false) String agencyIn, Model theModel) {
+		theModel.addAttribute("agency", advertServiceImpl.minPriceForNeighbourhood(agencyIn));
 		return "homepage";
 	}
-	
-	@RequestMapping(value="/3", method=RequestMethod.GET)
-	public String agencyAnalysis(Model theModel) {
-		theModel.addAttribute("agency", advertServiceImpl.minPriceForNeighbourhood("Имоти томов"));
-		return "homepage";
-	}
+
 }
