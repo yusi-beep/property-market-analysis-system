7696cbf2248d167276d1d6af9680e7103de91277
Yusuf Yusufov <yusitoo1234@gmail.com>
2022-05-15 12:43:37 +0300

    Add query for city list

--- src/main/java/com/real/estate/analyzer/repository/CityRepository.java
+++ src/main/java/com/real/estate/analyzer/repository/CityRepository.java
@@ -1,5 +1,8 @@
 package com.real.estate.analyzer.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,7 @@ import com.real.estate.analyzer.entities.City;
 public interface CityRepository extends CrudRepository<City, Long> {
 	
 	City getCityByName(String name);
+	
+//	@Query("SELECT name FROM city")
+//	public List<String> cityList();
 }
